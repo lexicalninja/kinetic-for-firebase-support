621550a2765012b4200395d7a69b4dd8518c5b38
Patrick Saxton <patrick@urban-intellect.com>
2018-06-28 11:29:22 -0500

    remove firestore deps

--- Fit/app/src/main/java/com/kinetic/fit/viewModels/ProfileViewModel.kt
+++ Fit/app/src/main/java/com/kinetic/fit/viewModels/ProfileViewModel.kt
@@ -4,27 +4,27 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FirebaseFirestore
+//import com.google.firebase.firestore.DocumentSnapshot
+//import com.google.firebase.firestore.FirebaseFirestore
 
 
 class ProfileViewModel : ViewModel() {
     private var user: MutableLiveData<Map<String, Any>> = MutableLiveData()
     private var name: MutableLiveData<String> = MutableLiveData()
-    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+//    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
     private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
     init {
-        db.collection("/profiles").document(mAuth.currentUser?.uid!!).addSnapshotListener{snapshot, e ->
-            if(e != null) {
-                Log.d("ProfileViewModel", e.toString())
-                return@addSnapshotListener
-            } else if (snapshot != null && snapshot.exists()) {
-                Log.d("ProfileViewModel", "Current data: " + snapshot.data)
-                user.postValue(snapshot.data)
-            } else {
-                Log.d("ProfileViewModel", "Current data: null");
-            }
-        }
+//        db.collection("/profiles").document(mAuth.currentUser?.uid!!).addSnapshotListener{snapshot, e ->
+//            if(e != null) {
+//                Log.d("ProfileViewModel", e.toString())
+//                return@addSnapshotListener
+//            } else if (snapshot != null && snapshot.exists()) {
+//                Log.d("ProfileViewModel", "Current data: " + snapshot.data)
+//                user.postValue(snapshot.data)
+//            } else {
+//                Log.d("ProfileViewModel", "Current data: null");
+//            }
+//        }
     }
 
     fun getName(): MutableLiveData<String> {
